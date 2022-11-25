1bf67858449c871072355db500fbfa81c8c3eb36
ElenaG <freshelf@yandex.ru>
2022-11-25 21:54:53 +0500

    add task2

---
+++ task_2.java
@@ -0,0 +1,9 @@
+// Вывести все простые числа от 1 до 1000.
+
+public class task_2 {
+    public static void main(String[] args){
+        for (int i = 1; i < 1001; i++){
+            System.out.printf("%d ", i);
+        }
+    }
+}
